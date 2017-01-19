6b45b8bd33a315186144631d5e374080220e5b87
kartaggen <kartaggen@email.com>
2017-01-19 14:17:47 +0200

    Add files via upload

---
+++ 81300/zad2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+};
+
+template <typename T>
+void sameElems(Node * root1, Node * root2) {
+	map<T, bool> elems;
+	stack<Node> current;
+	current.push_back(root1);
+	while (!current.empty()) {
+		Node * currentNode = current.top();
+		elems.insert(currentNode->data);
+		current.pop();
+		if (currentNode->left != nullptr) {
+			current.push_back(currentNode->left);
+		}
+		if (current.top()->right != nullptr) {
+			current.push_back(currentNode->right);
+		}
+	}
+	current.push_back(root2);
+	while (!current.empty()) {
+		Node * currentNode = current.top();
+		if (!(elems.find(currentNode->data)==elems.end())){
+			cout << currentNode->data;
+		}
+		current.pop();
+		if (currentNode->left != nullptr) {
+			current.push_back(currentNode->left);
+		}
+		if (current.top()->right != nullptr) {
+			current.push_back(currentNode->right);
+		}
+
+	}
+}
